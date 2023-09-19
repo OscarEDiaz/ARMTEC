62c0ce1127440d1cb032c07e4bf8e5400da06b87
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-09-19 11:17:02 -0600

    Finished mqtt config frontend

--- src/components/dashboard/dashboard_config/ConfigPage.jsx
+++ src/components/dashboard/dashboard_config/ConfigPage.jsx
@@ -10,12 +10,39 @@ export const ConfigPage = () => {
         PASSWORD: '',
     });
 
+    const onMqttConfigChange = ({target: {value, name}}) => {
+        setMqttConfig({
+            ...mqttConfig,
+            [ name ]: value
+        })
+    }
+
+    const onTestButtonPressed = () => {
+        
+    }
+
+    const onSaveButtonPressed = () => {
+
+    }
+
     return (
         <div className="config-container">
             <h1 className='config-header'>Configuration</h1>
-            <div className="mqtt-config-options">
-                <form action="" method="post">
-                    
+            <div className="config-options">
+                <h className="general-header" >MQTT Configuration</h>
+                <form className="mqtt-config" action="" method="post">
+                    <label htmlFor="mqttUrl">MQTT URL</label>
+                    <input className='general-input' onChange={onMqttConfigChange} type="url" name="MQTTURL" id="mqttUrl" value={mqttConfig['MQTTURL']} />
+                    <label htmlFor="mqttPort">Port</label>
+                    <input className='general-input' onChange={onMqttConfigChange} type="text" name="PORT" id="mqttPort" value={mqttConfig['PORT']} />
+                    <label htmlFor="mqttUsername">Username</label>
+                    <input className='general-input' onChange={onMqttConfigChange} type="text" name="USER" id="mqttUsername" value={mqttConfig['USER']} />
+                    <label htmlFor="mqttPassword">Password</label>
+                    <input className='general-input' onChange={onMqttConfigChange} type="password" name="PASSWORD" id="mqttPassword" value={mqttConfig['PASSWORD']} />
+                    <div className="mqtt-config-btns">
+                        <input type="button" value="Test Configuration" />
+                        <input type="button" value="Save Configuration" />
+                    </div>
                 </form>
             </div>
         </div>
