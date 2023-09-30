eb14eb6bc81bfd61911c5650b9a14a85c6d8eabf
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-09-30 14:27:45 -0600

    Fixed event propagation

--- src/components/dashboard/data_telemetry/DataTelemetry.jsx
+++ src/components/dashboard/data_telemetry/DataTelemetry.jsx
@@ -6,8 +6,13 @@ import { SensorConfig } from "./SensorConfig";
 import '../../../styles/telemetry.css';
 
 export const DataTelemetry = () => {
+    // MQTT DATA STATES
     const [topic, payload] = useGetSensorData();
+    
+    // SEARCH BAR STATES
     const [filter, setFilter] = useState('');
+
+    // COMPONENTS DATA FROM DB STATES
     const [componentsData, setComponentsData] = useState([
         {
             sensor: 'Preassure',
@@ -26,52 +31,54 @@ export const DataTelemetry = () => {
         }
     ]);
 
+    // ADD SENSOR INTERFACE STATES
     const [showConfiguration, setShowConfiguration] = useState(false);
     
+    // Handle filter change on search bar
     const onFilterChange = (event) => {
         setFilter(event.target.value);
     }
 
+    // Handle add sensor button pressing
     const handleAddSensorPressed = () => {
         setShowConfiguration(!showConfiguration)
     }
 
     // Hide add sensor interface if the user clicks everywhere but the configuration panel
-    const handleTelemetryContainerPressed = () => {
-        if (showConfiguration)
-            setShowConfiguration(!showConfiguration)
+    const handleTelemetryContainerPressed = (e) => {
+        if (e.target === e.currentTarget) {
+            showConfiguration && setShowConfiguration(!showConfiguration);
+        }
     }
 
-    return (
-        <>        
-            <div className="telemetry-container" onClick={handleTelemetryContainerPressed} >
-                <div className="telemetry-navbar">
-                    <h1 className="telemetry-title">Data Telemetry</h1>
-                    <input className="telemetry-searchbar" placeholder="Buscar" value={filter} onChange={onFilterChange} type="text" />
-                </div>
-                <div className="telemetry-cards-container" >
-                    {
-                        componentsData.map((component) => {
-                            if (component.sensor.toLowerCase().includes(filter.toLowerCase()))
-                                return <DataComponent 
-                                    key={component.sensor} 
-                                    title={component.sensor.toUpperCase()}
-                                    sensor={component.sensor}
-                                    measure={component.measure.toUpperCase()} 
-                                    chartType={component.chartType} 
-                                    payload={topic === component.sensor ? payload : null}   
-                                />
-                            else
-                                return null
-                        })
-                    }
-                </div>
+    return (      
+        <div className="telemetry-container" onClick={handleTelemetryContainerPressed} >
+            <div className="telemetry-navbar">
+                <h1 className="telemetry-title">Data Telemetry</h1>
+                <input className="telemetry-searchbar" placeholder="Buscar" value={filter} onChange={onFilterChange} type="text" />
+            </div>
+            <div className="telemetry-cards-container" >
+                {
+                    componentsData.map((component) => {
+                        if (component.sensor.toLowerCase().includes(filter.toLowerCase()))
+                            return <DataComponent 
+                                key={component.sensor} 
+                                title={component.sensor.toUpperCase()}
+                                sensor={component.sensor}
+                                measure={component.measure.toUpperCase()} 
+                                chartType={component.chartType} 
+                                payload={topic === component.sensor ? payload : null}   
+                            />
+                        else
+                            return null
+                    })
+                }
             </div>
             <SensorConfig isVisible={showConfiguration} />
             <button className="add-sensor-btn" onClick={handleAddSensorPressed}>
                 {/* Replace with icon */}
                 +
             </button>
-        </>
+        </div>
     )
 }
