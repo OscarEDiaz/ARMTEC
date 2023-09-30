a3cd735a6833f1e544fee8d01ae25a3d1b24f61d
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-09-30 07:50:13 -0600

    Added visibility toggles for the add sensor interface

--- src/components/dashboard/data_telemetry/DataTelemetry.jsx
+++ src/components/dashboard/data_telemetry/DataTelemetry.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { DataComponent } from "./DataComponent";
 import { useGetSensorData } from "../../../hooks/dashboard/useGetSensorData";
+import { SensorConfig } from "./SensorConfig";
 
 import '../../../styles/telemetry.css';
 
@@ -24,13 +25,24 @@ export const DataTelemetry = () => {
             chartType: 'Line'
         }
     ]);
+
+    const [showConfiguration, setShowConfiguration] = useState(false);
     
     const onFilterChange = (event) => {
         setFilter(event.target.value);
     }
 
+    const handleAddSensorPressed = () => {
+        setShowConfiguration(!showConfiguration)
+    }
+
+    const handleTelemetryContainerPressed = () => {
+        if (showConfiguration)
+            setShowConfiguration(!showConfiguration)
+    }
+
     return (
-        <div className="telemetry-container">
+        <div className="telemetry-container" onClick={handleTelemetryContainerPressed} >
             <div className="telemetry-navbar">
                 <h1 className="telemetry-title">Data Telemetry</h1>
                 <input className="telemetry-searchbar" placeholder="Buscar" value={filter} onChange={onFilterChange} type="text" />
@@ -52,6 +64,11 @@ export const DataTelemetry = () => {
                     })
                 }
             </div>
+            <SensorConfig isVisible={showConfiguration} />
+            <button className="add-sensor-btn" onClick={handleAddSensorPressed}>
+                {/* Replace with icon */}
+                +
+            </button>
         </div>
     )
 }
