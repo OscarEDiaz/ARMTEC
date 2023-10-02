4092eaedbe2ce40bac7b549df31bef66943ddf25
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-10-02 14:14:04 -0600

    Changed order of elements in add sensor interface

--- src/components/dashboard/data_telemetry/SensorConfig.jsx
+++ src/components/dashboard/data_telemetry/SensorConfig.jsx
@@ -5,12 +5,12 @@ export const SensorConfig = ({isVisible}) => {
         <div className={`sensor-props ${isVisible ? 'show' : 'hide'}`}>
             <h1>Add new sensor</h1>
             <form className='sensor-props-form' action="">
-                <label htmlFor="">Sensor Name</label>
-                <input type="text" name="" id="" />
-                <label htmlFor="">Color</label>
-                <input type="color" name="" id="" />
-                <label htmlFor="">Topic</label>
-                <input type="text" name="" id="" />
+                <label htmlFor="">Title</label>
+                <input type="text" name="" id="title-prop" />
+                <label htmlFor="topic-prop">Topic</label>
+                <input type="text" name="" id="topic-prop" />
+                <label htmlFor="color-prop">Color</label>
+                <input type="color" name="" id="color-prop" />
             </form>
         </div>
     )
