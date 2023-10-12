152644907441a9622a507b1e5149a9771d352480
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-10-12 14:51:18 -0600

    Created logic to communicate neighbour cards data

--- src/components/dashboard/data_telemetry/DataTelemetry.jsx
+++ src/components/dashboard/data_telemetry/DataTelemetry.jsx
@@ -6,11 +6,17 @@ import { SensorConfig } from "./SensorConfig";
 import { useGetSensors } from "../../../hooks/dashboard/useGetSensors";
 
 import '../../../styles/telemetry.css';
+import { useRef } from "react";
+import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 export const DataTelemetry = () => {
     // MQTT DATA STATES
     const [refresh, setRefresh] = useState(false);
-    const [components, isLoading] = useGetSensors(refresh, setRefresh);
+    const {components, setComponents, isFetching} = useGetSensors(refresh, setRefresh);
+
+    const [isLoadingCards, setIsLoadingCards] = useState(true);
+    const [cardsData, setCardsData] = useState([]);
     
     // SEARCH BAR STATES
     const [filter, setFilter] = useState('');
@@ -35,6 +41,19 @@ export const DataTelemetry = () => {
         }
     }
 
+    useEffect(() => {
+        if (!isFetching) {
+            console.log('Components: ', components)
+            setCardsData(components);
+            setIsLoadingCards(false);
+        }
+    }, [isFetching, components])
+
+
+    useEffect(() => {
+        console.log('Global cards data: ', JSON.parse(JSON.stringify(cardsData)))
+    }, [cardsData])
+
     return (      
         <div className="telemetry-container" onClick={handleTelemetryContainerPressed} >
             <div className="telemetry-navbar" onClick={handleTelemetryContainerPressed} >
@@ -43,12 +62,15 @@ export const DataTelemetry = () => {
             </div>
             <div className="telemetry-cards-container" onClick={handleTelemetryContainerPressed} >
                 {
-                    isLoading 
+                    isLoadingCards 
                     ? <h1>LOADING</h1>
-                    : components.map((component) => {
+                    : components.map((component, index) => {
                         if (component.title.toLowerCase().includes(filter.toLowerCase()))
                             return <DataComponent 
                                 key={component.id}
+                                neighboursData={cardsData}
+                                registerMeasure={setCardsData}
+                                currentIndex={index}
                                 id={component.id}
                                 title={component.title}
                                 topic={component.topic}
